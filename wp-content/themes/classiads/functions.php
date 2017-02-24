a449262cdab5cb65dbcbc5fb384b5737e3730264
Jack Cheng <chengxianga2008@yahoo.com>
2017-02-25 00:04:00 +0800

    update

--- wp-content/themes/classiads/functions.php
+++ wp-content/themes/classiads/functions.php
@@ -1555,4 +1555,13 @@ function remove_admin_bar() {
 	}
 }
 
+// remove wp version param from any enqueued scripts
+function vc_remove_wp_ver_css_js( $src ) {
+	if ( strpos( $src, 'ver=' ) )
+		$src = remove_query_arg( 'ver', $src );
+		return $src;
+}
+add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+
 include_once "custom_settings.php";
