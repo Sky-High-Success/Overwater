935bb2465db46d002015736a6465c02367fa009c
Jack Cheng <chengxianga2008@yahoo.com>
2017-03-02 09:57:58 +0800

    update

--- wp-content/themes/classiads/functions.php
+++ wp-content/themes/classiads/functions.php
@@ -27,7 +27,7 @@
 
 global $delegate_urls;
 
-$delegate_urls = array('http://luxuryislandresorts.com.au','http://tahitinuivacations.com');
+$delegate_urls = array('http://luxuryislandresorts.com.au','http://tahitiholiday.com.au');
 
 add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin' );
 
